6033f44c99b33d86c409cdfaeaccb6279e3dcbdc
dfmiquel <diego.miquel@gmail.com>
2021-12-25 09:43:57 -0300

    Desafio Clase 03 - JSX

--- src/App.js
+++ src/App.js
@@ -1,23 +1,13 @@
-import logo from './logo.svg';
 import './App.css';
+import './components/navBar/navBar.css';
+import { NavBar } from './components/navBar/navBar';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <h1>MovieFan</h1>
-        <p>
-          La Página de los Fanáticos de las Películas!!!!!
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <NavBar/>
+
       </header>
     </div>
   );
---
+++ src/components/navBar/navBar.js
@@ -0,0 +1,25 @@
+export const NavBar = () => {
+    return (
+        <nav className="navBar">
+            <h1>MovieFan</h1>
+            <p>
+                La Página de los Fanáticos de las Películas!!!!!
+            </p>
+
+            <html>
+                <head>
+                </head>
+                <body>
+                    <ul>
+                        <li><a href="#">Inicio</a></li>
+                        <li><a href="#">Películas</a></li>
+                        <li><a href="#">Posters</a></li>
+                        <li><a href="#">Coleccionables</a></li>
+                        <li><a href="#">Sobre Nosotros</a></li>
+                    </ul>
+                </body>
+            </html>
+
+        </nav>
+    )
+}
