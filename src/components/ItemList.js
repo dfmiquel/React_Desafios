825e5eead9e5eac8bad779e6f4d422560f69b62c
dfmiquel <diego.miquel@gmail.com>
2022-02-10 21:10:43 -0300

    Clase06-PROMESAS-MAP

---
+++ src/components/ItemList.js
@@ -0,0 +1,20 @@
+import Item from "./Item";
+
+const ItemList = ({ products }) => {
+  return (
+    <div>
+      {products.map((product) => {
+        return (
+          <Item
+            key={product.id}
+            title={product.title}
+            pictureUrl={product.pictureUrl}
+            price={product.price}
+            description={product.description}
+          />
+        );
+      })}
+    </div>
+  );
+};
+export default ItemList;
--- src/components/ItemListContainer.jsx
+++ src/components/ItemListContainer.jsx
@@ -1,13 +1,32 @@
 
-import ItemCount from "./ItemCount.jsx";
+import { useState, useEffect } from "react";
+// import getData from "../Services/getData.js";
+// import ItemCount from "./ItemCount.jsx";
+import ItemList from "./ItemList";
+import axios from "axios";
 
 const ItemListContainer = ({ greeting }) => {
+  const [products, setProducts] = useState([]);
+
+  const getProducts = async () => {
+    const getDataProducts = await axios.get(
+      "https://franncode.vercel.app/api/products"
+    );
+    const productsResponse = getDataProducts.data;
+    setProducts(productsResponse);
+  };
+  useEffect(() => {
+    getProducts();
+  }, []);
 
   return (
     <div>
       <h1>{greeting}</h1>
-      <ItemCount stock={10} initial={1} />
+      <ItemList products={products} />
+      {/* <ItemCount stock={10} initial={1} /> */}
     </div>
   );
+
+
 };
 export default ItemListContainer
